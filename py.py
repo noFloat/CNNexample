9fd7dd168f71b079226d1956f46d9b10ff4ed2b0
nofloat <304546210@qq.com>
2017-11-06 21:36:53 +0800

    kersa

--- py.py
+++ py.py
@@ -1,5 +1,12 @@
 import numpy as np
 import sys
+import keras
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D, Flatten
+from keras.optimizers import Adam
+
 path='1.txt'
 def process_line(line):
     lineSpilt = line.split(' ',1)
@@ -31,8 +38,19 @@ for line in f:
 
 
 f.close()
+model = Sequential()
 
+model.add(Dense(units=len(X), input_dim=115))
+model.add(Activation('relu'))
+#model.add(Dense(units=115,input_dim=1))
+model.add(Activation('softmax'))
 
+model.compile(loss='categorical_crossentropy',
+                         optimizer='sgd',
+                         metrics=['accuracy'])
+model.compile(loss=keras.losses.categorical_crossentropy,
+                         optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
 
-
-
+# model.fit(X,Y,epochs=1252,batch_size=200)
+#
+# loss_and_metrics=model.evaluate(X,Y,batch_size=128)
