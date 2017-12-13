76a118e563002ac6c0e58d75f2dacf7fa02cfe58
nofloat <304546210@qq.com>
2017-12-13 14:40:15 +0800

    location

--- STLCSSDistance.py
+++ STLCSSDistance.py
@@ -153,7 +153,7 @@ def load(path):
             chance[i] += matrix_mid[i][j]
 
     mid_mat=[matrix_mid,matrix_chance]
-    # Matrix.append(mid_mat)
+    Matrix.append(mid_mat)
 
     #转移矩阵结束
     Path.append(path)
