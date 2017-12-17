74cd37e1bf8d44599fa4f3bd4b9bc655a9a853ae
nofloat <304546210@qq.com>
2017-12-17 17:33:07 +0800

    cnn

--- cnn.py
+++ cnn.py
@@ -1,87 +1,62 @@
+# -*- coding: utf-8 -*-
 import numpy as np
-import os,string,re
+import os, string, re
 import theano
 import sys
 import keras
-from keras.layers.core import  Reshape,RepeatVector,Dropout
+from keras.layers.core import Reshape, RepeatVector, Dropout
 from keras.datasets import mnist
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import *
 from keras.callbacks import EarlyStopping
 
-import goal_address
+# import goal_address
 np.random.seed(1337)
-path='1.txt'
-db = goal_address.connectdb()
+path = '/home/liwenjie/liurong/wordvec/data/10km.txt'
+# db = goal_address.connectdb()
 
 pattern = re.compile('P')
+category=33
 
 def file_name(file_dir):
-         for root, dirs, files in os.walk(file_dir):
-             return files
+    for root, dirs, files in os.walk(file_dir):
+        return files
+
 
 def process_line(line):
-    tmp=line.strip('\n').rstrip().split(',')
+    tmp = line.strip('\n').rstrip().split(',')
     return tmp
     sys.exit()
-    lineSpilt = line.split(' ',1)
-
-
-    str1="[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+"
-    str2=""
+    lineSpilt = line.split(' ', 1)
+    str1 = "[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+"
+    str2 = ""
     string = re.sub(str1, str2, lineSpilt[0])
 
+    if (line != ''):
 
-    if(string!=''):
-
-        goalLine=lineSpilt[1]
+        goalLine = line
         tmp = [float(val) for val in goalLine.strip('\n').rstrip().split(' ')]
         x = np.array(tmp[0:])
-        for i in(range(100-len(x))):
+        for i in (range(100 - len(x))):
             x.append(0)
 
-        #sys.exit()
+        # sys.exit()
         return tmp
     else:
         return 0
 
-#原来的
-def process_line2(line):
-    lineSpilt = line.split(' ',1)
-
-
-    str1="[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+"
-    str2=""
-    string = re.sub(str1, str2, lineSpilt[0])
-
-
-    if(string!=''):
-
-        goalLine=lineSpilt[1]
-        tmp = [float(val) for val in goalLine.strip('\n').rstrip().split(' ')]
-        x = np.array(tmp[0:])
-        for i in(range(100-len(x))):
-            x.append(0)
-
-        #sys.exit()
-        return tmp
-    else:
-        return 0
 
 X = []
 Y = []
 
 
-
 def load(path):
-    f = open("./screen_content/"+path,encoding='utf-8')
+    f = open(path)
     type = 1
     mid = []
     list = [0.0 for i in range(60)]
-
     for line in f:
-
         if line.strip() == '':
             type = 1
             for i in range(100 - len(mid)):
@@ -89,140 +64,74 @@ def load(path):
             X.append(mid)
             mid = []
             continue
-
         if type == 1:
             Y.append(line.strip('\n').rstrip())
             type = 0
         else:
-
             x = process_line(line)
-
-            if(x!=0):
+            if (x != 0):
                 mid.append(x)
-
     f.close()
 
-files=file_name("./screen_content/")
+
+# files = file_name("data/")
+files = [path]
 for file in files:
-    if(file!='.DS_Store'):
+    if (file != '.DS_Store'):
         load(file)
 
-
-X1=X[:int(len(X)/2)]
-X2=X[int(len(X)/2)+1:]
-Y1=Y[:int(len(Y)/2)]
-Y2=Y[int(len(Y)/2)+1:]
+X1 = X[:int(len(X) * 0.8)]
+X2 = X[int(len(X) * 0.8):]
+Y1 = Y[:int(len(Y) * 0.8)]
+Y2 = Y[int(len(Y) * 0.8):]
 X1 = np.array(X1)
-Y1 = np.array(Y1)
 X2 = np.array(X2)
-Y2 = np.array(Y2)
 
-
-X1=X1.reshape(len(X1),100,60)
-X2=X2.reshape(len(X2),100,60)
-
-model = Sequential()
+X1 = X1.reshape(len(X1), 100, 60)
+X2 = X2.reshape(len(X2), 100, 60)
+Y1 = keras.utils.to_categorical(Y1, category)
+Y2 = keras.utils.to_categorical(Y2, category)
 
 nb_words = min(7000, len(X1))
 embedding_matrix = np.zeros((nb_words + 1, 60))
-
-# embedding_layer = Embedding(nb_words+1,
-#                             200,
-#                             input_length=920,
-#                             weights=[embedding_matrix],
-#                             trainable=True)
-
-embedding_layer = Embedding(nb_words+1,
+embedding_layer = Embedding(nb_words + 1,
                             60,
-                            input_shape=(100,60),
+                            input_shape=(100, 60),
                             weights=[embedding_matrix],
                             trainable=True)
-model_left = Sequential()
+print('Training model.')
 
-model_3 = Sequential()
-model_3.add(embedding_layer)
-model_3.add(Conv2D(200, 6, activation='tanh'))
-model_3.add(MaxPooling2D(3))
-model_3.add(Conv2D(200, 6, activation='tanh'))
-model_3.add(MaxPooling2D(3))
-# model_3.add(Conv1D(200, 6, activation='tanh'))
-# model_3.add(MaxPooling1D(30))
-model_3.add(Flatten())
-
-#model_3.summary()
-
-# model_3 = Sequential()
-# model_3.add(Conv1D(200, 3, activation='tanh',input_shape=(100,200)))
-# model_3.add(MaxPooling1D(3))
-# model_3.add(Conv1D(200, 3, activation='tanh'))
-# model_3.add(MaxPooling1D(3))
-# model_3.summary()
-
-#merged = Merge([model_left,model_3],concat_axis=1)
-
-
-model.add(Dense(units=200,batch_input_shape=(32,100,60)))
-
-#model.add(model_3)
-
+model = Sequential()
+# model.add(Dense(units=200, input_shape=(100, 60)))
 model.add(Conv1D(60,
-                 5,
+                 3,
                  padding='same',
                  activation='tanh',
-                 input_shape=(32,100)))
-
-
-# model.add(Conv1D(200, 2,
-#                         padding='causal',
-#                  input_shape=(2,100,200))) # 卷积层1
-#model.add(Activation('relu')) #激活层
-
-model.add(MaxPooling1D(pool_size=1)) #池化层
+                 input_shape=(100, 60)
+                 ))
+model.add(MaxPooling1D(pool_size=2))  # 池化层
 model.add(Dropout(0.25))
-#model.add(Flatten()) #拉成一维数据
-
-
 model.add(Conv1D(60,
-                 5,
+                 2,
                  padding='same',
                  activation='tanh',
-                 input_shape=(32,60)))
-model.add(MaxPooling1D(pool_size=1)) #池化层
-model.add(Dropout(0.25))
-model.add(Flatten()) #拉成一维数据
-
-
-
-model.summary()
-#model.add(Reshape((100, 200)))
-#model.add(Dense(160)) #全连接层1
-model.add(Activation('relu')) #激活层
+                 ))
+model.add(MaxPooling1D(pool_size=2))  # 池化层
+model.add(Activation('relu'))  # 激活层
 model.add(Dropout(0.5))
-model.add(Dense(646)) #全连接层2
-model.add(Activation('softmax'))
-
-#
+model.add(Flatten())
+model.add(Dense(category, activation='softmax'))  # 全连接层2
 model.summary()
-#
-#
-#
-# model.add(Activation('linear'))
-# #
-# model.add(Flatten())
-# #
-# model.add(Reshape((100, 200)))
-# #model.add(Dense(1))
-# model.add(Activation('linear'))
-# sgd = keras.optimizers.SGD(lr=0.005, decay=1e-6, momentum=0.9, nesterov=True)
-# model.compile(loss='categorical_crossentropy',optimizer=sgd,metrics=['accuracy'])
+sgd = keras.optimizers.SGD(lr=0.005, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+early_stopping = EarlyStopping(monitor='val_loss', patience=2)
 
-model.compile(loss='mean_squared_error',
-                          optimizer=keras.optimizers.Adadelta())
+tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
 
-early_stopping =EarlyStopping(monitor='val_loss', patience=2)
-model.fit(X1,Y1,epochs=1, batch_size=32)
-#
+model.fit(X1, Y1, epochs=50, batch_size=32, callbacks=[tbCallBack])
+
 loss, accuracy = model.evaluate(X2, Y2)
-
+classes = model.predict_classes(X2, batch_size=32) #5预测模型
+proba = model.predict_proba(X2, batch_size=32)
 print('\ntest loss: ', loss)
 print('\ntest accuracy: ', accuracy)
